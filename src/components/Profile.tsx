f301d111240eae63a5520629e891935ca790fa24
Dorgival Rocha <dojak220@ufrn.edu.br>
2021-02-25 17:09:46 -0300

    Fix Challenge Context in Profile.tsx

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,10 @@
+import { useContext } from "react";
+import { ChallengesContext } from "../contexts/ChallengesContext";
 import styles from "../styles/components/Profile.module.css";
 
 export function Profile() {
+   const { level } = useContext(ChallengesContext)
+
    return (
       <div className={styles.profileContainer}>
          <img src="https://github.com/dojak220.png" alt="Dorgival Rocha" />
@@ -9,8 +13,8 @@ export function Profile() {
             <p>
                {/* O next identifica que esse caminho em src está em public/ */}
                <img src="icons/level.svg" alt="Level" />
-               Level 1
-               </p>
+               Level {level}
+            </p>
          </div>
       </div>
    );
